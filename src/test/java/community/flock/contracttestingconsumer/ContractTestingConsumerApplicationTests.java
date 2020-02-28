0db6790c4df044525bb2028c90fa3f26845ea890
Julius van Dis <vandis.j@gmail.com>
2020-02-28 10:54:51 +0100

    Spring initializer start contract-testing-consumer

---
+++ src/test/java/community/flock/contracttestingconsumer/ContractTestingConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package community.flock.contracttestingconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractTestingConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
